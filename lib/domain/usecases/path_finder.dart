dda607e8f728f11018d3ca1ad4a8a5a473bebbc5
hhhan-jh <jeo4924@gmail.com>
2025-11-21 04:49:56 +0900

    Comment: 주석 수정
    시작과 끝의 경계가 모호하여 start/end 추가함

--- lib/domain/usecases/path_finder.dart
+++ lib/domain/usecases/path_finder.dart
@@ -88,7 +88,7 @@ class PathFinder {
     List<int> fullPath = [];
     double totalCost = 0.0;
 
-    debugPrint('----------- [findPathWithWaypoints] -----------');
+    debugPrint('----------- [findPathWithWaypoints Start] -----------');
     // [출발, 경유1, 경유2, 도착] 리스트를 순회하며 구간별 경로 계산
     for (int i = 0; i < orderedVertexIds.length - 1; i++) {
       final startId = orderedVertexIds[i];
@@ -116,7 +116,7 @@ class PathFinder {
       debugPrint('구간 $i 성공: 비용 ${result.totalCost}, 길이 ${result.path.length}');
     }
     debugPrint('-- 경로 탐색 완료: 총비용 $totalCost, 총길이 ${fullPath.length}');
-    debugPrint('----------- [findPathWithWaypoints] -----------');
+    debugPrint('----------- [findPathWithWaypoints End] -----------');
     return PathResult(path: fullPath, totalCost: totalCost);
   }
 }
